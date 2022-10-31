259593c68f20bac0cb7ec642031219e4a16d8e77
skrasek.michael <skrasek.michal@email.cz>
2022-10-31 22:42:51 +0100

    fixed installing dependencies, added gegl & glm dependencies

--- libs/install.ps1
+++ libs/install.ps1
@@ -4,18 +4,28 @@ function Install {
 		$Name
 	)
 
+	if (Test-Path $Name -PathType Container) {
+		Write-Host "$Name already installed"
+		return
+	}
+
 	$cache = "$PSScriptRoot/.download-cache"
-	if (![System.IO.Directory]::Exists($cache)) {
+	if (!(Test-Path $cache -PathType Container)) {
 		New-Item -ItemType Directory $cache | Out-Null
 	}
 
-	if (![System.IO.File]::Exists("$cache/$Name")) {
-		Write-Host "Downloading $Name ..."
-		Invoke-WebRequest $Url -Method "GET" -Outfile $cache/$Name
+	$fileName = [System.IO.Path]::GetFileName($Url);
+	if (!(Test-Path "$cache/$Name" -PathType Leaf)) {
+		Write-Host "Downloading $Name [$fileName]..."
+		Invoke-WebRequest $Url -Method "GET" -Outfile "$cache/$fileName"
+	} else {
+		Write-Host "$Name [$fileName] loaded from cache"
 	}
 
-	Write-Host "Extracting $Name ..."
-	Expand-Archive $cache/$Name -DestinationPath $PSScriptRoot
+	Write-Host "Extracting $Name [$fileName]..."
+	Expand-Archive "$cache/$fileName" -DestinationPath $PSScriptRoot -Force
 }
 
-Install -Name "SDL-release-2.24.1.zip" -Url "https://github.com/libsdl-org/SDL/archive/refs/tags/release-2.24.1.zip"
+Install -Name "SDL-release-2.24.1" -Url "https://github.com/libsdl-org/SDL/archive/refs/tags/release-2.24.1.zip"
+Install -Name "glm" -Url "https://github.com/g-truc/glm/releases/download/0.9.9.8/glm-0.9.9.8.zip"
+Install -Name "geGL-master" -Url "https://github.com/dormon/geGL/archive/refs/heads/master.zip"
