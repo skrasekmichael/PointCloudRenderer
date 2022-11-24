7ff86ed60d724b4abcc43a6c1a97515d9993d3c4
skrasek.michael <skrasek.michal@email.cz>
2022-11-24 17:32:31 +0100

    added OpenTK window

---
+++ src/PointCloudRenderer.App/AppWindow.cs
@@ -0,0 +1,26 @@
+﻿using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+namespace PointCloudRenderer.App;
+
+public class AppWindow : GameWindow
+{
+	public AppWindow(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings)
+	{
+	}
+
+	protected override void OnLoad()
+	{
+		base.OnLoad();
+	}
+
+	protected override void OnRenderFrame(FrameEventArgs args)
+	{
+		base.OnRenderFrame(args);
+	}
+
+	protected override void OnUnload()
+	{
+		base.OnUnload();
+	}
+}
