8a8a73d05f87be2d652da7299de209dd7ec26fa0
skrasek.michael <skrasek.michal@email.cz>
2022-11-28 13:27:25 +0100

    refactored to WPF with OpenTK.GLWpfControl

---
+++ src/PointCloudRenderer.App/App.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using PointCloudRenderer.APP.Views;
+using System.Windows;
+
+namespace PointCloudRenderer.APP;
+
+public partial class App : Application
+{
+	private readonly IHost host;
+
+	public App()
+	{
+		host = Host.CreateDefaultBuilder()
+			.ConfigureAppConfiguration(ConfigureAppConfiguration)
+			.ConfigureServices((context, services) => ConfigureServices(context.Configuration, services))
+			.Build();
+	}
+
+	private static void ConfigureAppConfiguration(HostBuilderContext context, IConfigurationBuilder builder)
+	{
+	}
+
+	private static void ConfigureServices(IConfiguration configuration, IServiceCollection services)
+	{
+		services.AddSingleton<MainWindow>();
+	}
+
+	protected override async void OnStartup(StartupEventArgs e)
+	{
+		await host.StartAsync();
+
+		var window = host.Services.GetRequiredService<MainWindow>();
+		window.Show();
+
+		base.OnStartup(e);
+	}
+
+	protected override async void OnExit(ExitEventArgs e)
+	{
+		using (host)
+		{
+			await host.StopAsync(TimeSpan.FromSeconds(5));
+		}
+
+		base.OnExit(e);
+	}
+}
